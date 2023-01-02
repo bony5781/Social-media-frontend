16f5acd25ec212f53a51909d3725f4aa94ee94ed
bony5781 <bhowmikabhinav@gmail.com>
2023-01-02 20:16:04 +0530

    Final

--- src/Context/AuthReducer.js
+++ src/Context/AuthReducer.js
@@ -36,7 +36,7 @@ const AuthReducer = (state, action) => {
             };
         case "LOGOUT":
             return {
-                user: JSON.parse(localStorage.setItem("user",null)),
+                user: localStorage.setItem("user",null),
                 isFetching: false,
                 error: false
             };
