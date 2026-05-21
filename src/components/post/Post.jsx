eae55773756c29e7fb737c46bd403b4d49154f09
Abhinav Bhowmik <89747136+bony5781@users.noreply.github.com>
2026-05-21 15:41:35 +0530

    Update Post.jsx

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,78 +1,160 @@
 import './post.css'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { axiosInstance } from '../../config';
 import { format } from "timeago.js"
 import { Link } from 'react-router-dom'
-import { useContext } from 'react';
 import { AuthContext } from '../../Context/AuthContext';
 
 function Post({ post }) {
 
     const [like, setLike] = useState(post.like.length);
-    const [isLiked, setisLiked] = useState(false);
+    const [isLiked, setIsLiked] = useState(false);
     const [user, setUser] = useState({});
+
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
     const { user: currentUser } = useContext(AuthContext);
 
     useEffect(() => {
-        setisLiked(post.like.includes(currentUser._id));
-    }, [currentUser._id, post.like])
+        setIsLiked(post.like.includes(currentUser._id));
+    }, [currentUser._id, post.like]);
 
     useEffect(() => {
+
         const fetchUsers = async () => {
-            const res = await axiosInstance.get(`/users/?userId=${post.userId}`);
-            setUser(res.data);
+
+            try {
+
+                const res = await axiosInstance.get(`/users/?userId=${post.userId}`);
+
+                setUser(res.data);
+
+            } catch (err) {
+                console.log(err);
+            }
         }
+
         fetchUsers();
+
     }, [post.userId]);
 
-    const likeHandler = () => {
-        try {
-            axiosInstance.put("/posts/" + post._id + "/like", { userId: currentUser._id });
-        } catch (err) {
+    const likeHandler = async () => {
 
+        try {
+
+            await axiosInstance.put(
+                "/posts/" + post._id + "/like",
+                { userId: currentUser._id }
+            );
+
+            setLike(isLiked ? like - 1 : like + 1);
+
+            setIsLiked(!isLiked);
+
+        } catch (err) {
+            console.log(err);
         }
-        setLike(isLiked ? like - 1 : like + 1);
-        setisLiked(!isLiked);
     }
 
     return (
+
         <div className="post">
+
             <div className="postWrapper">
+
                 <div className="postTop">
+
                     <div className="postTopLeft">
-                        <Link to={`profile/${user.username}`}>
+
+                        <Link to={`/profile/${user.username}`}>
+
                             <img
                                 className="postProfileImg"
-                                src={user.profilePicture ? user.profilePicture : PF + "person/noAvatar.png"}
+                                src={
+                                    user.profilePicture
+                                        ? user.profilePicture
+                                        : PF + "person/noAvatar.png"
+                                }
                                 alt=""
                             />
+
                         </Link>
-                        <span className="postUsername">
-                            {user.username}
-                        </span>
-                        <span className="postDate">{format(post.createdAt)}</span>
+
+                        <div className="postUserInfo">
+
+                            <span className="postUsername">
+                                {user.username}
+                            </span>
+
+                            <span className="postDate">
+                                {format(post.createdAt)}
+                            </span>
+
+                        </div>
+
                     </div>
+
                     <div className="postTopRight">
-                        <MoreVertIcon />
+                        <MoreVertIcon className="postMenuIcon" />
                     </div>
+
                 </div>
+
                 <div className="postCenter">
-                    <span className="postText">{post?.desc}</span>
-                    <img className="postImg" src={post.img} alt="" />
+
+                    {post?.desc && (
+                        <span className="postText">
+                            {post.desc}
+                        </span>
+                    )}
+
+                    {post.img && (
+                        <img
+                            className="postImg"
+                            src={post.img}
+                            alt=""
+                        />
+                    )}
+
                 </div>
+
                 <div className="postBottom">
+
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src={`${PF}like.png`} onClick={likeHandler} alt="" />
-                        <img className="likeIcon" src={`${PF}heart.png`} onClick={likeHandler} alt="" />
-                        <span className="postLikeCounter">{like} people like it</span>
+
+                        <img
+                            className={`likeIcon ${isLiked ? "liked" : ""}`}
+                            src={`${PF}like.png`}
+                            onClick={likeHandler}
+                            alt=""
+                        />
+
+                        <img
+                            className={`likeIcon ${isLiked ? "liked" : ""}`}
+                            src={`${PF}heart.png`}
+                            onClick={likeHandler}
+                            alt=""
+                        />
+
+                        <span className="postLikeCounter">
+                            {like} people like this
+                        </span>
+
                     </div>
+
                     <div className="postBottomRight">
-                        <span className="postCommentText">{post.comment} comments</span>
+
+                        <span className="postCommentText">
+                            {post.comment} comments
+                        </span>
+
                     </div>
+
                 </div>
+
             </div>
+
         </div>
     )
 }
