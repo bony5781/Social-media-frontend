b80c8eb2ebd0df6c022f1a9564f655d6250bf924
Abhinav Bhowmik <89747136+bony5781@users.noreply.github.com>
2026-05-21 16:08:28 +0530

    Update Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,9 @@
 import './sidebar.css'
+
 import { axiosInstance } from '../../config';
+
 import CloseFriend from '../closeFriend/CloseFriend';
+
 import RssFeedIcon from '@mui/icons-material/RssFeed';
 import ChatIcon from '@mui/icons-material/Chat';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
@@ -10,78 +13,120 @@ import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
 import EventIcon from '@mui/icons-material/Event';
 import SchoolIcon from '@mui/icons-material/School';
-import { AuthContext } from '../../Context/AuthContext';
-import { useState } from 'react';
-import { useContext, useEffect } from 'react';
 
-function Sidebar({ user }) {
+import { AuthContext } from '../../Context/AuthContext';
+
+import { useState, useContext, useEffect } from 'react';
+
+function Sidebar() {
 
   const [friends, setFriends] = useState([]);
-  const { user: currentUser} = useContext(AuthContext);
+
+  const { user: currentUser } = useContext(AuthContext);
 
   useEffect(() => {
+
     const getFriends = async () => {
+
       try {
-        const friendList = await axiosInstance.get("/users/friends/" + currentUser._id);
+
+        const friendList = await axiosInstance.get(
+          "/users/friends/" + currentUser._id
+        );
+
         setFriends(friendList.data);
+
       } catch (err) {
+
         console.log(err);
+
       }
     };
-    getFriends();
-  }, [user, currentUser._id])
 
+    getFriends();
+
+  }, [currentUser._id]);
 
   return (
+
     <div className='sidebar'>
+
       <div className="sidebarWrapper">
+
         <ul className="sidebarList">
+
           <li className="sidebarListItem">
             <RssFeedIcon className='sidebarIcon' />
             <span className="sidebarListItemText">Feed</span>
           </li>
+
           <li className="sidebarListItem">
             <ChatIcon className='sidebarIcon' />
             <span className="sidebarListItemText">Chat</span>
           </li>
+
           <li className="sidebarListItem">
             <PlayCircleIcon className='sidebarIcon' />
-            <span className="sidebarListItemText">Video</span>
+            <span className="sidebarListItemText">Videos</span>
           </li>
+
           <li className="sidebarListItem">
             <GroupIcon className='sidebarIcon' />
             <span className="sidebarListItemText">Groups</span>
           </li>
+
           <li className="sidebarListItem">
             <BookmarkIcon className='sidebarIcon' />
-            <span className="sidebarListItemText">Bookmark</span>
+            <span className="sidebarListItemText">Bookmarks</span>
           </li>
+
           <li className="sidebarListItem">
             <QuestionMarkIcon className='sidebarIcon' />
             <span className="sidebarListItemText">Questions</span>
           </li>
+
           <li className="sidebarListItem">
             <WorkHistoryIcon className='sidebarIcon' />
             <span className="sidebarListItemText">Jobs</span>
           </li>
+
           <li className="sidebarListItem">
             <EventIcon className='sidebarIcon' />
             <span className="sidebarListItemText">Events</span>
           </li>
+
           <li className="sidebarListItem">
             <SchoolIcon className='sidebarIcon' />
             <span className="sidebarListItemText">Courses</span>
           </li>
+
         </ul>
-        <button className="sidebarButton">Show More</button>
+
         <hr className='sidebarHr' />
-        <ul className="sidebarFriendList">
-          <h6>Your Friends </h6>
-          {friends.map((u) => (
-            <CloseFriend key={u._id} user={u} />
-          ))}
-        </ul>
+
+        <div className="sidebarFriendsSection">
+
+          <h6 className="sidebarTitle">
+            Your Friends
+          </h6>
+
+          <ul className="sidebarFriendList">
+
+            {friends.map((u) => (
+
+              <CloseFriend
+                key={u._id}
+                user={u}
+              />
+
+            ))}
+
+          </ul>
+
+        </div>
+
       </div>
+
     </div>
   )
 }
